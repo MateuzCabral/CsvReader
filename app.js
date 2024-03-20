3aa18733ba3aeaac038669c2d024edc93933d4ee
mateus.cabral <mateuscabral7575@gmail.com>
2024-03-20 15:46:14 -0300

    Adicionado Leitura de CSV

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const fs = require("fs");
+const csv = require("csv");
+
 const app = express();
 
-app.get("/", async (req, res) => {
-  return res.send("Importação concluida.");
+app.get("/", (req, res) => {
+  const arquivoCsv = "arquivo.csv"; //Necessario especificar o caminho do arquivo csv
+
+  fs.createReadStream(arquivoCsv).pipe(
+    csv.parse({
+      columns: true,
+      delimiter: ';',
+    })
+  ).on('data',async (dadosLinha) =>{
+    var dados = dadosLinha;
+    console.log(dados);
+  });
+
+  return res.status(200).send("Importação concluida");
 });
 
 const PORT = process.env.PORT || 8080;
